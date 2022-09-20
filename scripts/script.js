f5fe83ad001f0831e0b2460526bef069cc12c2c7
rudnam <francisalbarracin2@gmail.com>
2022-09-20 18:31:37 +0800

    Allow clicking

--- scripts/script.js
+++ scripts/script.js
@@ -7,7 +7,9 @@ const gameBoard = (() => {
                  '','',''];
     const getBoard = () => board;
     const modifyBoard = (index, value) => {
-        board[index] = value;
+        if (!board[index]) {
+            board[index] = value;
+        }
     }
     return {getBoard,
             modifyBoard};
@@ -15,18 +17,49 @@ const gameBoard = (() => {
 
 const Player = (name, mark) => {
     const getName = () => name;
-    const move = index => {
-        gameBoard.modifyBoard(index,mark);
-    }
+    const getMark = () => mark;
     return {getName,
-            move};
+            getMark};
 };
+
+const Game = (() => {
+    let ongoing = false;
+    let P1 = null;
+    let P2 = null;
+    let turn = null;
+    const start = (Player1,Player2) => {
+        if (!ongoing) {
+            ongoing = true;
+            P1 = Player1;
+            P2 = Player2;
+            turn = P1;
+            for (let i=0; i < 9; i++) {
+                cells[i].addEventListener('click', function() {
+                    Game.move(cells[i].dataset.index);
+                });
+            }
+        }
+    }
+    const move = index => {
+        gameBoard.modifyBoard(index,turn.getMark());
+        renderContent();
+        turn = turn.getName() === P1.getName() ? P2 : P1;
+    }
+    const getTurn = () => turn.getName();
+    return {start,
+            move,
+            getTurn};
+})();
 
 function renderContent() {
     const board = gameBoard.getBoard();
     for (let i=0; i < 9; i++) {
-        cells[i].innerText = board[i] ? board[i] : '😂';
+        cells[i].innerText = board[i];
     }
 }
 
-renderContent();
+
+// test
+const test1 = Player('rudnam','🤣');
+const test2 = Player('bot','🤬');
+Game.start(test1, test2);
