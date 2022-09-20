f76b4e330076ab8e371ac0d03bc43b7b738a1880
rudnam <francisalbarracin2@gmail.com>
2022-09-20 17:12:11 +0800

    Set up functions

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,22 @@
 // script.js
+
+const gameBoard = (() => {
+    let board = ['','','',
+                 '','','',
+                 '','',''];
+    const getBoard = () => board;
+    const modifyBoard = (index, value) => {
+        board[index] = value;
+    }
+    return {getBoard,
+            modifyBoard};
+})();
+
+const Player = (name, mark) => {
+    const getName = () => name;
+    const move = index => {
+        gameBoard.modifyBoard(index,mark);
+    }
+    return {getName,
+            move};
+};
